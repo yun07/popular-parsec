bb0b6a78d4c79315eef486a290f2a35e169bf6dc
yun07 <heungyun@hotmail.com.com>
2026-04-02 23:51:05 +0800

    video OK

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,28 +1,84 @@
 export const projects = [
   {
-    title: "Procedural FX",
-    slug: "procedural-fx",
-    image: "/assets/artwork1.jpg", 
-    video: "JdFvGFVXsZQ",  // YouTube ID
-    description: "Procedural FX tool with Niagara and Unreal Engine."
+    title: "Procedural Assembly Framework",
+    slug: "Procedural Assembly Framework",
+    image: "/assets/Artboard1.png", 
+    video: "HRG_wxbH-JM",  // YouTube ID
+    description: [
+      "Project Problem:", 
+      "Particles too expensive in large scenes",
+      "Solution:",
+      "- Custom GPU simulation stage",
+      "- LOD particle system",
+      "- Distance-based spawn control",
+      "Result:", 
+      "Performance improved 60%"
+    ]
   },
   {
-    title: "Water Simulation",
-    slug: "water-sim",
-    image: "/assets/artwork2.jpg",
-    video: "KiKDFJKJFK",  // Vimeo ID
-    description: "Procedural water simulation in Unreal Engine."
+    title: "Runtime Procedural Building",
+    slug: "Runtime Procedural Building",
+    image: "/assets/Artboard2.png", 
+    video: "obk82-dtMSI",  // YouTube ID
+    description: [
+      "Project Problem:", 
+      "Particles too expensive in large scenes",
+      "Solution:",
+      "- Custom GPU simulation stage",
+      "- LOD particle system",
+      "- Distance-based spawn control",
+      "Result:", 
+      "Performance improved 60%"
+    ]
   },
   {
-    title: "Destruction Tool",
-    slug: "destruction",
-    image: "/assets/artwork3.jpg",
-    description: "Destruction system with procedural effects."
+    title: "Runtime Skinning Tool",
+    slug: "Runtime Skinning Tool",
+    image: "/assets/Artboard3.png", 
+    video: "oGftHyKeSig",  // YouTube ID
+    description: [
+      "Project Problem:", 
+      "Particles too expensive in large scenes",
+      "Solution:",
+      "- Custom GPU simulation stage",
+      "- LOD particle system",
+      "- Distance-based spawn control",
+      "Result:", 
+      "Performance improved 60%"
+    ]
+  },
+    {
+    title: "Houdini Landscape Tool",
+    slug: "Houdini Landscape Tool",
+    image: "/assets/Artboard5.png", 
+    video: "WWqp0Zsk1co",  // YouTube ID
+    description: [
+      "Project Problem:", 
+      "Particles too expensive in large scenes",
+      "Solution:",
+      "- Custom GPU simulation stage",
+      "- LOD particle system",
+      "- Distance-based spawn control",
+      "Result:", 
+      "Performance improved 60%"
+    ]
   },
   {
-    title: "aaa",
-    slug: "xxx",
-    image: "/assets/artwork2.jpg",
-    description: "xxx"
+    title: "Wave Function Collapse",
+    slug: "Wave Function Collapse",
+    image: "/assets/Artboard6.png", 
+    video: "jQ0PkM4OCrM",  // YouTube ID
+    description: [
+      "Project Problem:", 
+      "Particles too expensive in large scenes",
+      "Solution:",
+      "- Custom GPU simulation stage",
+      "- LOD particle system",
+      "- Distance-based spawn control",
+      "Result:", 
+      "Performance improved 60%"
+    ]
   },
+  
+  
 ];
